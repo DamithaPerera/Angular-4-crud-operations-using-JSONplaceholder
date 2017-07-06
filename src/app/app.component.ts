c1c0cadef14f4f9a8de9f8ea4485bfbe6f092045
damithap <damithap@ellipsis.lk>
2017-07-06 17:52:47 +0530

    all

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Http } from "@angular/http";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+posts:any[];
+private url='https://jsonplaceholder.typicode.com/posts';
+
+   constructor(private http:Http){
+     http.get(this.url)
+     .subscribe(response => {
+       this.posts=response.json();
+     })
+   }
+
+
+   createPost(input: HTMLInputElement){
+        let post={title:input.value};
+        input.value='';
+        this.http.post(this.url,JSON.stringify(post))
+        .subscribe(response =>{
+            post['id']=response.json().id;
+            this.posts.splice(0,0,post);
+            console.log(response.json());
+        });
+   }
+
+
+   updatePost(post){
+    this.http.patch(this.url+'/'+post.id,JSON.stringify({isRead:true}))
+    .subscribe(response=>{
+      console.log(response.json());
+    })
+   }
+
+
+   deletePost(post){
+     this.http.delete(this.url+'/'+post.id)
+     .subscribe(response=>{
+       let index = this.posts.indexOf(post);
+       this.posts.splice(index,1);
+     })
+   }
+
 }
